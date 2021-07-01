97f03b29f827b92b11e512f99780d7134eca2018
Raul Villalobos <stuart123460@yahoo.com>
2021-07-01 10:03:47 -0700

    Create computerPlay function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function computerPlay() {
+    let num = Math.random();
+    let result;
+    if (num < 0.3333) {
+        result = 'Rock';
+    } else if (num < 0.6666) {
+        result = 'Paper';
+    } else {
+        result = 'Scissors';
+    }
+    return result;
+}
+
+console.log(computerPlay());
